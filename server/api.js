62d3599917e3de43a7fe44522a7fc83d3c8f4845
raviasaini <ravia.saini81@outlook.com>
2023-09-06 21:29:28 +0100

    characters endpoint working

--- server/api.js
+++ server/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const logger = require('morgan')
+const db = require('./db/connect.js'); 
 
 const api = express();
 
@@ -14,5 +15,18 @@ api.get("/", (req, res) => {
         description: "Collect them all!"
     })
 })
+
+api.get('/cards', async (req, res) => {
+
+    try {
+      const results = await db.query('SELECT * FROM aniChar');
+      
+      res.json(results.rows);
+  
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error fetching aniChar' });
+    }
+  });
 
 module.exports = api;
